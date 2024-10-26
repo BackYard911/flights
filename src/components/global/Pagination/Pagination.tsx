12b2540e7dbed4533cb5973a131ffcf9ecf22079
Youssef <yousseftarek9801@gmail.com>
2024-10-26 14:38:09 +0300

    feat: use react-select

--- src/components/global/Pagination/Pagination.tsx
+++ src/components/global/Pagination/Pagination.tsx
@@ -1,38 +1,87 @@
+import Select, { CSSObjectWithLabel } from "react-select";
 import { IPaginationProps } from "./pagination.types";
+import { PAGE_SIZE_OPTIONS } from "../../../consts/generic";
+import styles from "./pagination.module.css";
 
-function Pagination ({ currentPage, totalPages, pageSize, onPageChange, onPageSizeChange }: IPaginationProps) {
-    const handlePageChange = (page: number) => {
-        onPageChange(page);
-    };
+function Pagination({
+  currentPage,
+  totalPages,
+  pageSize,
+  onPageChange,
+  onPageSizeChange,
+}: IPaginationProps) {
+  const SelectStyles = {
+    container: (provided: CSSObjectWithLabel) => ({
+      ...provided,
+      width: "5em",
+      margin: "0.5em auto",
+    }),
+    control: (provided: CSSObjectWithLabel) => ({
+      ...provided,
+      background: "#1A1A1A",
+      borderColor: "#1A1A1A",
+      borderRadius: "0.5em",
+      color: "white",
+      "&:hover": {
+        borderColor: "#1A1A1A",
+      },
+    }),
+    menu: (provided: CSSObjectWithLabel) => ({
+      ...provided,
+      background: "#1A1A1A",
+      color: "white",
+      "&:hover": {
+        borderColor: "#1A1A1A",
+      },
+    }),
+    singleValue: (provided: CSSObjectWithLabel) => ({
+      ...provided,
+      color: "white",
+    }),
+  };
+  const handlePageChange = (page: number) => {
+    onPageChange(page);
+  };
 
-    return (
-        <div>
-            <div>
-                <label>
-                    Page size:
-                    <select
-                        value={pageSize}
-                        onChange={(event) => onPageSizeChange(Number(event.target.value))}
-                    >
-                        <option value={10}>10</option>
-                        <option value={20}>20</option>
-                        <option value={50}>50</option>
-                    </select>
-                </label>
-            </div>
-            <div>
-                <button disabled={currentPage === 1} onClick={() => handlePageChange(currentPage - 1)}>Previous</button>
-                <span>{currentPage}</span>
-                <button disabled={currentPage === totalPages} onClick={() => handlePageChange(currentPage + 1)}>Next</button>
-            </div>
-            <div>
-                <label>
-                    Total pages:
-                    <span>{totalPages}</span>
-                </label>
-            </div>
+  return (
+    <div className={styles.pagination}>
+      <div className={styles["pagination-buttons-container"]}>
+        <div className={styles["pagination-buttons"]}>
+          <button
+            disabled={currentPage === 1}
+            onClick={() => handlePageChange(currentPage - 1)}
+          >
+            Previous
+          </button>
+          <span>{currentPage}</span>
+          <button
+            disabled={currentPage === totalPages}
+            onClick={() => handlePageChange(currentPage + 1)}
+          >
+            Next
+          </button>
         </div>
-    );
+        <div>
+        <label>
+          Total pages:
+          <span>{totalPages}</span>
+        </label>
+      </div>
+      </div>
+      <div>
+        <label>
+          <Select
+            options={PAGE_SIZE_OPTIONS}
+            onChange={(value) => onPageSizeChange(value?.value as number)}
+            defaultValue={{ value: pageSize, label: pageSize.toString() }}
+            styles={SelectStyles}
+            />
+            Page size
+        </label>
+
+      </div>
+    </div>
+  );
 }
 
-export default Pagination
+export default Pagination;
---
+++ src/consts/generic.ts
@@ -0,0 +1,7 @@
+const PAGE_SIZE_OPTIONS = [
+  { value: 10, label: "10" },
+  { value: 20, label: "20" },
+  { value: 50, label: "50" },
+];
+
+export { PAGE_SIZE_OPTIONS }
